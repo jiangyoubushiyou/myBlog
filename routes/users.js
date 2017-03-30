f234a93e505cfc2f30ee0f9d2246fad97eaac02e
jiangyoubushiyou <563108916@qq.com>
2017-03-30 22:31:16 +0800

    req.params.name

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function (req, res) {
-    res.send('hello, ' + req.parame.name);
+    res.send('hello, ' + req.params.name);
 });
 module.exports = router;
