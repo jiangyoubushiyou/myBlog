694e7a7e98b67727d358eaa00083f96435619132
jiangyoubushiyou <563108916@qq.com>
2017-04-01 22:42:10 +0800

    views

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function (req, res) {
+    res.render('users', {
         name: req.params.name
     });//����res.render������Ⱦejsģ�壬res.render ��һ��������ģ������֣������� users ���ƥ�� views/users.ejs
 });//res.render �����þ��ǽ�ģ������ݽ������ html
